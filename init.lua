80a85fb97c04e7ca5a9e074ab38e5d5d8ba10a45
Long Nguyen <me@long-nguyen.dev>
2024-05-27 11:28:06 -0500

    configure neovide

--- init.lua
+++ init.lua
@@ -3,5 +3,13 @@ require("config.lazy")
 
 if vim.g.neovide then
   vim.g.neovide_window_blurred = true
-  vim.g.neovide_show_border = true
+  vim.g.neovide_transparency = 0.9
+
+  local padding = 16
+  vim.g.neovide_padding_top = padding + 16
+  vim.g.neovide_padding_bottom = padding
+  vim.g.neovide_padding_right = padding
+  vim.g.neovide_padding_left = padding
+
+  vim.o.guifont = "GeistMono Nerd Font"
 end
