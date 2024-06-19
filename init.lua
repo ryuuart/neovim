cd03a5d7dad211ebc66d7232cc200cb91e6baecc
Long Nguyen <18nguyenl@gmail.com>
2024-06-18 22:38:28 -0500

    feat: remove transparency
    too buggy right now

--- init.lua
+++ init.lua
@@ -3,7 +3,6 @@ require("config.lazy")
 
 if vim.g.neovide then
   vim.g.neovide_window_blurred = true
-  vim.g.neovide_transparency = 0.9
 
   local padding = 16
   vim.g.neovide_padding_top = padding + 16
