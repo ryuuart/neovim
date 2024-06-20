4a0cf4cc35d0b93413ba7b33f852aa32fdba57fb
Long Nguyen <18nguyenl@gmail.com>
2024-06-20 01:49:52 -0500

    create new neovim start

---
+++ lua/config/neovide.lua
@@ -0,0 +1,12 @@
+if vim.g.neovide then
+  vim.g.neovide_window_blurred = true
+  vim.g.neovide_transparency = 0.9
+
+  local padding = 16
+  vim.g.neovide_padding_top = padding + 16
+  vim.g.neovide_padding_bottom = padding
+  vim.g.neovide_padding_right = padding
+  vim.g.neovide_padding_left = padding
+
+  vim.o.guifont = "GeistMono Nerd Font Mono,GeistMono Nerd Font:h16"
+end
---
+++ lua/plugins/nvim-treesitter.lua
@@ -0,0 +1,40 @@
+return {
+  {
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    auto_install = true,
+    opts = {
+      highlight = { enable = true },
+      indent = { enable = true },
+      ensure_installed = {
+        "bash",
+        "c",
+        "diff",
+        "html",
+        "javascript",
+        "jsdoc",
+        "json",
+        "jsonc",
+        "lua",
+        "luadoc",
+        "luap",
+        "markdown",
+        "markdown_inline",
+        "printf",
+        "python",
+        "query",
+        "regex",
+        "toml",
+        "tsx",
+        "typescript",
+        "vim",
+        "vimdoc",
+        "xml",
+        "yaml",
+      },
+    },
+    config = function(_, opts)
+      require("nvim-treesitter.configs").setup(opts)
+    end,    
+  },
+}
